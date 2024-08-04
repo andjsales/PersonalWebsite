0ce0ee73da12f5abee2dde34d825393488ae2e1f
andrew.sales <andjsales@gmail.com>
2024-08-03 20:01:18 -0500

    Set up Express server to serve static files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname)));
+
+// Handle all other routes by sending the main HTML file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
